217536384408c8f08d6c961afcaa976bfbcfeafd
0scar-M <21omccartney@wakatipu.school.nz>
2024-04-19 15:51:29 +1200

    Revert "Orbiscape v0.4"
    This reverts commit 9a80f9ed9a7ea85bae0296c0d5fe7b94a784d4a4.

--- Orbiscape v0.4/Scripts/Levels.cs
+++
@@ -1,8 +0,0 @@
-using Godot;
-using System;
-using System.Collections.Generic;
-
-public partial class Levels : Node
-{
-	public static Dictionary<int, LevelFile> LevelFiles = new Dictionary<int, LevelFile> {};
-}
